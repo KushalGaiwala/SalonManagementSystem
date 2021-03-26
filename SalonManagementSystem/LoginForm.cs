f48f76a3e7c4b60267a4b1185894e2a54dcef073
Kushal Gaiwala <kushalGaywala@hotmail.com>
2021-03-26 15:34:56 +0530

    Added Login Verification in LoginForm

--- SalonManagementSystem/LoginForm.cs
+++ SalonManagementSystem/LoginForm.cs
@@ -17,10 +17,23 @@ namespace SalonManagementSystem
         {
             InitializeComponent();
         }
-
+        // Add your connection string
+        private SqlConnection con = new SqlConnection(@"Data Source=KUSHAL\MSSQLSERVER01;Initial Catalog=system;Integrated Security=True");
+        private SqlCommand cmd;
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            
+            cmd = new SqlCommand("select LId from tblLoginDetail where UName='"+txtName.Text+"' and UPass='"+txtPassword.Text+"'",con);
+            con.Open();
+            int i = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+            if (i == 0)
+            {
+                MessageBox.Show("Not Logged in!");
+            }
+            else
+            {
+                MessageBox.Show("Use is logged in!");
+            }
         }
     }
 }
